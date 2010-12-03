fbe6e13eb87564bd110ab53c743bae07cb26ec7e
Cyrille Pontvieux <jrdATenialisDOTnet>
2010-12-03 14:09:46 +0000

    First version to work !

--- create-iso.sh
+++ create-iso.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-[ $UID -eq 0 ] || exit 1
 cd $(dirname $0)
-./create-initrd.sh && cp kernel/boot/vmlinuz-* iso/isolinux/vmlinuz && cp initrd.gz iso/isolinux/initrd.gz && mkisofs -R -J -V SaLT -b isolinux/isolinux.bin -c isolinux/isolinux.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o salt.iso iso && chown jrd:users salt.iso 
+[ "$1" = "debug" ] && debug=debug || debug=
+./create-initrd.sh && cp kernel/boot/vmlinuz-* iso/isolinux/vmlinuz && cp initrd.gz iso/isolinux/initrd.gz && sed -i "s:\(.*/dev/ram0\).*:\1 $debug:" iso/isolinux/isolinux.cfg && mkisofs -R -J -V SaLT -b isolinux/isolinux.bin -c isolinux/isolinux.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o salt.iso iso
